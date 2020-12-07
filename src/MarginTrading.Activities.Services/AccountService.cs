d1c2c850b17b5e4bb03806c177d9ac0fdb105470
Iliyan Iliev <Iliyan.Iliev@primeholding.com>
2020-12-07 14:26:34 +0200

    Lt-2872 Add account name to description attributes if present

---
+++ src/MarginTrading.Activities.Services/AccountService.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) 2019 Lykke Corp.
+// See the LICENSE file in the project root for more information.
+
+using System.Threading.Tasks;
+using MarginTrading.AccountsManagement.Contracts;
+using MarginTrading.Activities.Services.Abstractions;
+
+namespace MarginTrading.Activities.Services
+{
+    public class AccountService : IAccountsService
+    {
+        private readonly IAccountsApi _accountsApi;
+
+        public AccountService(IAccountsApi accountsApi)
+        {
+            _accountsApi = accountsApi;
+        }
+
+        public async Task<string> GetAccountNameByAccountId(string id)
+        {
+            var response = await _accountsApi.GetById(id);
+
+            return response?.AccountName;
+        }
+    }
+}
