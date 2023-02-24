357dad7bab2f4b645a110052941a65a896178614
Fatih Dumanli <fatihdumanli@live.com>
2023-02-24 09:51:15 +0300

    feat(LT-4517): add more unit tests

--- tests/MarginTrading.Activities.Tests/CashMovementProjectionTests.cs
+++ tests/MarginTrading.Activities.Tests/CashMovementProjectionTests.cs
@@ -14,12 +14,47 @@ namespace MarginTrading.Activites.Tests
         private Mock<IActivitiesSender> _activitySenderMock;
         private Mock<IIdentityGenerator> _identityGeneratorMock;
 
-        private static DepositSucceededEvent DepositSucceededEvent = new DepositSucceededEvent(Guid.NewGuid().ToString(), DateTime.UtcNow, "", "", 55.2m);
+        private static DepositSucceededEvent DepositSucceededEvent = 
+            new DepositSucceededEvent(Guid.NewGuid().ToString(), DateTime.UtcNow, "clientId1", "accountId1", 55.2m);
+
+        private static DepositFailedEvent DepositFailedEvent = 
+            new DepositFailedEvent(Guid.NewGuid().ToString(), DateTime.UtcNow, "clientId2", "accountId2", 1000.0m);
+
+        private static WithdrawalSucceededEvent WithdrawalSucceededEvent = 
+            new WithdrawalSucceededEvent(Guid.NewGuid().ToString(), DateTime.UtcNow, "clientId3", "accountId3", 500.55m);
+
+        private static WithdrawalFailedEvent WithdrawalFailedEvent = 
+            new WithdrawalFailedEvent(Guid.NewGuid().ToString(), DateTime.UtcNow, "reason", "accountId4", "clientId4", 650.30m);
+
         private static IEnumerable<TestCaseData> DepositSucceededTestCase
         {
             get 
             {
                 yield return new TestCaseData(DepositSucceededEvent);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> DepositFailedTestCase
+        {
+            get 
+            {
+                yield return new TestCaseData(DepositFailedEvent);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> WithdrawalSucceededTestCase
+        {
+            get 
+            {
+                yield return new TestCaseData(WithdrawalSucceededEvent);
+            }
+        }
+
+        private static IEnumerable<TestCaseData> WithdrawalFailedTestCase
+        {
+            get 
+            {
+                yield return new TestCaseData(WithdrawalFailedEvent);
             }
         }
 
@@ -47,6 +82,66 @@ namespace MarginTrading.Activites.Tests
                     a.EventSourceId == e.AccountId &&
                     a.Timestamp == e.EventTimestamp && 
                     a.Event == Activities.Core.Domain.ActivityType.AccountDepositSucceeded && 
+                    a.DescriptionAttributes[0] == e.Amount.ToString())));
+        }
+
+        [Test, TestCaseSource(nameof(DepositFailedTestCase))]
+        public void DepositFailed_ShouldResultIn_PublishingAccurateActivity(DepositFailedEvent e)
+        {
+            var activityId = "activityId2";
+            _identityGeneratorMock.Setup(o => o.GenerateId()).Returns(activityId);
+            
+            var projection = CreateSut(_activitySenderMock.Object, _identityGeneratorMock.Object);
+            projection.Handle(e);
+            
+            _activitySenderMock.Verify(m => m.PublishActivity(
+                 It.Is<IActivity>(
+                    a => a.AccountId == e.AccountId &&
+                    a.Id == activityId &&
+                    a.Instrument == string.Empty &&
+                    a.EventSourceId == e.AccountId &&
+                    a.Timestamp == e.EventTimestamp && 
+                    a.Event == Activities.Core.Domain.ActivityType.AccountDepositFailed && 
+                    a.DescriptionAttributes[0] == e.Amount.ToString())));
+        }
+
+        [Test, TestCaseSource(nameof(WithdrawalSucceededTestCase))]
+        public void WithdrawalSucceeded_ShouldResultIn_PublishingAccurateActivity(WithdrawalSucceededEvent e)
+        {
+            var activityId = "activityId3";
+            _identityGeneratorMock.Setup(o => o.GenerateId()).Returns(activityId);
+            
+            var projection = CreateSut(_activitySenderMock.Object, _identityGeneratorMock.Object);
+            projection.Handle(e);
+            
+            _activitySenderMock.Verify(m => m.PublishActivity(
+                 It.Is<IActivity>(
+                    a => a.AccountId == e.AccountId &&
+                    a.Id == activityId &&
+                    a.Instrument == string.Empty &&
+                    a.EventSourceId == e.AccountId &&
+                    a.Timestamp == e.EventTimestamp && 
+                    a.Event == Activities.Core.Domain.ActivityType.AccountWithdrawalSucceeded && 
+                    a.DescriptionAttributes[0] == e.Amount.ToString())));
+        }
+
+        [Test, TestCaseSource(nameof(WithdrawalFailedTestCase))]
+        public void WithdrawalFailed_ShouldResultIn_PublishingAccurateActivity(WithdrawalFailedEvent e)
+        {
+            var activityId = "activityId4";
+            _identityGeneratorMock.Setup(o => o.GenerateId()).Returns(activityId);
+            
+            var projection = CreateSut(_activitySenderMock.Object, _identityGeneratorMock.Object);
+            projection.Handle(e);
+            
+            _activitySenderMock.Verify(m => m.PublishActivity(
+                 It.Is<IActivity>(
+                    a => a.AccountId == e.AccountId &&
+                    a.Id == activityId &&
+                    a.Instrument == string.Empty &&
+                    a.EventSourceId == e.AccountId &&
+                    a.Timestamp == e.EventTimestamp && 
+                    a.Event == Activities.Core.Domain.ActivityType.AccountWithdrawalFailed && 
                     a.DescriptionAttributes[0] == e.Amount.ToString())));
         }
         
