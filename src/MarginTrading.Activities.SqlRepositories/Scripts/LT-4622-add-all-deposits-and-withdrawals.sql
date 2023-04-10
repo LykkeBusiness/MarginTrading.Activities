cc1fcdabc93eeb52a32a2140d8e8affe9cbd8d4d
Fatih Dumanli <fatihdumanli@live.com>
2023-04-10 16:17:09 +0300

    feat(LT-4622): add script to add past deposit/withdrawal operations

---
+++ src/MarginTrading.Activities.SqlRepositories/Scripts/LT-4622-add-all-deposits-and-withdrawals.sql
@@ -0,0 +1,31 @@
+-- Import all Deposit operations as Activities
+insert into Activities
+(Id, AccountId, EventSourceId, Timestamp, Category, Event, DescriptionAttributes, RelatedIds, CorrelationId)
+SELECT LOWER(REPLACE(CONVERT(NVARCHAR(128), NEWID()), '-', '')),
+ah.AccountId,
+ah.AccountId,
+ah.ChangeTimestamp,
+'CashMovement',
+'AccountDepositSucceeded',
+CONCAT('[', '"', ah.ChangeAmount, '"', ',', '"', accounts.BaseAssetId, '"', ']'),
+'[]',
+ah.CorrelationId
+FROM AccountHistory ah
+inner join MarginTradingAccounts accounts ON ah.AccountId = accounts.Id
+where ah.ReasonType = 'Deposit'
+
+-- Import all Withdrawal operations as Activities
+insert into Activities
+(Id, AccountId, EventSourceId, Timestamp, Category, Event, DescriptionAttributes, RelatedIds, CorrelationId)
+SELECT LOWER(REPLACE(CONVERT(NVARCHAR(128), NEWID()), '-', '')),
+ah.AccountId,
+ah.AccountId,
+ah.ChangeTimestamp,
+'CashMovement',
+'AccountWithdrawalSucceeded',
+CONCAT('[', '"', ABS(ah.ChangeAmount), '"', ',', '"', accounts.BaseAssetId, '"', ']'),
+'[]',
+ah.CorrelationId
+FROM AccountHistory ah
+inner join MarginTradingAccounts accounts ON ah.AccountId = accounts.Id
+where ah.ReasonType = 'Withdraw'
