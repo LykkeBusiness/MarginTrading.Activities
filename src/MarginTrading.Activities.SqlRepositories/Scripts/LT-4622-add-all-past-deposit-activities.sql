750317c66dd861420ad533f10e2f9303ae8d2372
Fatih Dumanli <fatihdumanli@live.com>
2023-04-10 16:19:44 +0300

    feat(LT-4622): add separate files

---
+++ src/MarginTrading.Activities.SqlRepositories/Scripts/LT-4622-add-all-past-deposit-activities.sql
@@ -0,0 +1,15 @@
+-- Import all Deposit operations as Activities
+insert into Activities
+(Id, AccountId, EventSourceId, Timestamp, Category, Event, DescriptionAttributes, RelatedIds, CorrelationId)
+SELECT LOWER(REPLACE(CONVERT(NVARCHAR(128), NEWID()), '-', '')),
+ah.AccountId,
+ah.AccountId,
+ah.ChangeTimestamp,
+'CashMovement',
+'AccountDepositSucceeded',
+CONCAT('[', '"', ah.ChangeAmount, '"', ',', '"', accounts.BaseAssetId, '"', ']'),
+'[]',
+ah.CorrelationId
+FROM AccountHistory ah
+inner join MarginTradingAccounts accounts ON ah.AccountId = accounts.Id
+where ah.ReasonType = 'Deposit'
