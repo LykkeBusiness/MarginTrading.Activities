e5e50b1b2f9130ac1adbc6d935442ecea4eab66a
Fatih Dumanli <fatihdumanli@live.com>
2023-04-10 16:47:35 +0300

    feat(LT-4622): add schema specification

--- src/MarginTrading.Activities.SqlRepositories/Scripts/LT-4622-add-all-past-withdrawal-activities.sql
+++ src/MarginTrading.Activities.SqlRepositories/Scripts/LT-4622-add-all-past-withdrawal-activities.sql
@@ -1,5 +1,5 @@
 -- Import all Withdrawal operations as Activities
-insert into Activities
+insert into [dbo].Activities
 (Id, AccountId, EventSourceId, Timestamp, Category, Event, DescriptionAttributes, RelatedIds, CorrelationId)
 SELECT LOWER(REPLACE(CONVERT(NVARCHAR(128), NEWID()), '-', '')),
 ah.AccountId,
@@ -10,11 +10,11 @@ ah.ChangeTimestamp,
 CONCAT('[', '"', ABS(ah.ChangeAmount), '"', ',', '"', accounts.BaseAssetId, '"', ']'),
 CONCAT('[', '"', ah.Id, '"', ']'),
 ah.CorrelationId
-FROM AccountHistory ah
-inner join MarginTradingAccounts accounts ON ah.AccountId = accounts.Id
+FROM [dbo].AccountHistory ah
+inner join [dbo].MarginTradingAccounts accounts ON ah.AccountId = accounts.Id
 where ah.ReasonType = 'Withdraw'
 AND NOT EXISTS (
     SELECT 1
-    FROM Activities
+    FROM [dbo].Activities
     WHERE RelatedIds = CONCAT('[', '"', ah.Id, '"', ']')
 );
