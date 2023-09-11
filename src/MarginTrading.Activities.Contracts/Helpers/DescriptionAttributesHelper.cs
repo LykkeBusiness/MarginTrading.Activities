90c85aea376efac4eb06c3f164c7eb685260b014
Aliaksandr Vashetsin <aliaksandr.vashetsin@lykke-business.ch>
2023-09-11 18:31:50 +0300

    LT-4977: more methods to support strings (for donut)

--- src/MarginTrading.Activities.Contracts/Helpers/DescriptionAttributesHelper.cs
+++ src/MarginTrading.Activities.Contracts/Helpers/DescriptionAttributesHelper.cs
@@ -8,26 +8,40 @@ namespace Lykke.MarginTrading.Activities.Contracts.Helpers
 {
     public static class DescriptionAttributesHelper
     {
+        public static HashSet<int> GetQtyIndexes(ActivityContract activity)
+        {
+            return GetQtyIndexes(activity.Category, activity.Event);
+        }
+        
         public static HashSet<int> GetQtyIndexes(ActivityCategoryContract category, ActivityTypeContract eventType)
         {
+            return GetQtyIndexes(category.ToString(), eventType.ToString());
+        }
+        
+        public static HashSet<int> GetQtyIndexes(string category, string eventType)
+        {
             var result = new HashSet<int>();
-            if (category == ActivityCategoryContract.Position)
+            if (category == ActivityCategoryContract.Position.ToString())
             {
                 result.Add(1);
             }
-            if (eventType == ActivityTypeContract.OrderModificationVolume)
+
+            if (eventType == ActivityTypeContract.OrderModificationVolume.ToString())
             {
                 result.Add(6);
                 result.Add(7);
             }
-            if (category == ActivityCategoryContract.Order)
+
+            if (category == ActivityCategoryContract.Order.ToString())
             {
                 result.Add(2);
             }
-            if (category == ActivityCategoryContract.Adjustment)
+
+            if (category == ActivityCategoryContract.Adjustment.ToString())
             {
                 result.Add(2);
             }
+
             return result;
         }
     }
