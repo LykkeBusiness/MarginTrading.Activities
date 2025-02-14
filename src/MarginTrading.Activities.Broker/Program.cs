c80a89ba02f4d86510758b3a85045a27810ae444
Dmitry Samodov <4929842+samodovdi@users.noreply.github.com>
2025-02-14 15:05:50 +0300

    LT-6051: don't use settings service

--- src/MarginTrading.Activities.Broker/Program.cs
+++ src/MarginTrading.Activities.Broker/Program.cs
@@ -11,7 +11,7 @@ namespace MarginTrading.Activities.Broker
     {
         public static void Main(string[] args)
         {
-            RunOnPort(5082, true);
+            RunOnPort(5082);
         }
     }
 }
